5569d5057995489c429a391aaa62b09fc32faaca
sunmuffin42 <sunkulp.ananth@gmail.com>
2023-02-11 14:08:59 -0500

    Add warning for mismatch, closes #4

--- parse-leipzig.js
+++ parse-leipzig.js
@@ -4,7 +4,26 @@ let parseLeipzig = word => {
 
   let formMorphemes = form.split(/[-=]/)
   let glossMorphemes = gloss.split(/[-=]/)
-
+  let warning = document.querySelector("#warning")
+  if (formMorphemes.length > glossMorphemes.length) {
+    // tell user that there is a mismatch
+    warning.innerHTML = 
+      `
+      <div id="warning">You have more morphemes in your form than in your gloss! Check to make sure it's even</div>
+      `
+  }
+  else {
+    if (formMorphemes.length < glossMorphemes.length) {
+      // tell user that there is a mismatch
+      warning.innerHTML =
+        `
+        <div id="warning">You have more morphemes in your gloss than in your form! Check to make sure it's even</div>
+        `
+    }
+    else {
+      warning.innerHTML = ``
+    }
+  }
   let morphemes = formMorphemes.map((formMorpheme, i) => {
     const morpheme = {
       form: formMorpheme,
@@ -12,8 +31,8 @@ let parseLeipzig = word => {
     }
     return morpheme
   })
-  
+
   return morphemes
 }
 
-export {parseLeipzig}
+export { parseLeipzig }
